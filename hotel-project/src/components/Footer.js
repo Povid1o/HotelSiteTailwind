228ded5f5a601e4a730ab8527610c0adf5f4bdf8
Povid1o <nikitka.bashlykov@mail.ru>
2024-05-02 12:24:49 +0300

    TrashComit

--- hotel-project/src/components/Footer.js
+++ hotel-project/src/components/Footer.js
@@ -10,7 +10,70 @@ const Footer = () => {
         </div> 
         
 
+<<<<<<< Updated upstream
      );
+=======
+    //  );
+
+     return (
+        <footer className="bg-main_theme shadow-2xl rounded-t-lg font-body mt-14 py-8">
+          <div className="container mx-auto px-4">
+            <div className="flex flex-wrap">
+                <div className="w-full md:w-1/4 mb-4 md:mb-0">
+                {/* <h2 className="text-lg text-[#F8F7F4] font-semibold mb-2">Винные Терассы</h2> */}
+                    <Logo className='w-[100px]'/>
+                </div>
+                {/* <div className="w-full md:w-1/4 mb-4 md:mb-0">
+                    <h2 className="text-lg font-semibold mb-2">Апартаменты</h2>
+                
+                </div>
+                <div className="w-full md:w-1/4 mb-4 md:mb-0">
+                    <h2 className="text-lg font-semibold mb-2">Как добраться</h2>
+                
+                </div>
+                <div className="w-full md:w-1/4 mb-4 md:mb-0">
+                    <h2 className="text-lg font-semibold mb-2">Контакты</h2>
+                
+                </div> */}
+            </div>
+            <hr className="border-gray-400 my-8" />
+            <div className="flex flex-wrap flex-row mx-auto">
+                <div className="w-full md:w-1/4 mb-4 md:mb-0">
+                    <h2 className="text-lg text-[#F8F7F4] font-semibold mb-2">Апартаменты</h2>
+                
+                </div>
+                <div className="w-full md:w-1/4 mb-4 md:mb-0">
+                    <h2 className="text-lg text-[#F8F7F4] font-semibold mb-2">Как добраться</h2>
+                
+                </div>
+                <div className="w-full md:w-1/4 mb-4 md:mb-0">
+                    <h2 className="text-lg text-[#F8F7F4] font-semibold mb-2">Контакты</h2>
+                
+                </div>
+                <div className="w-full md:w-1/4 mb-4 md:mb-0">
+                    <h2 className="text-lg text-[#F8F7F4] font-semibold mb-2">Отзывы</h2>
+                
+                </div>
+                <div className="w-full md:w-1/4 mb-4 md:mb-0">
+                    <h2 className="text-lg text-[#F8F7F4] font-semibold mb-2">Политика конфиденциальности</h2>
+                
+                </div>
+
+
+                {/* <div className="w-full md:w-1/3 my-4 mx-auto md:mb-0 text-center">
+                    <p className="text-[#F8F7F4]">© 2023 Винные Терассы</p>
+                </div> */}
+                {/* <div className="w-full md:w-1/3 mb-4 md:mb-0 text-center">
+                    <p className="text-gray-700">Политика конфиденциальности</p>
+                </div> */}
+            </div>
+            <div className="w-full md:w-1/3 my-4 mx-auto md:mb-0 text-center">
+                <p className="text-[#F8F7F4]">© 2023 Винные Терассы</p>
+            </div>
+          </div>
+        </footer>
+      );
+>>>>>>> Stashed changes
 }
  
 export default Footer;
