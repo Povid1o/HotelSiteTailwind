74f375b88fb50bb95d914f3ba989928f8e04ac34
Povid1o <nikitka.bashlykov@mail.ru>
2024-05-20 00:13:24 +0300

    Мероприятия v1
    Коммит будет аммендиться

--- hotel-project/src/Ivents.js
+++ hotel-project/src/Ivents.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import Navbar from "./components/Navbar.js"
+import Home from './components/Home'
+import Footer from "./components/Footer.js";
+import { useState } from "react";
+
 
 function Ivents() {
+
+    const [nav, setNav] = useState(false);
     return (
         <div>
-            Hi, you wanna have fun?
+            <Navbar nav = {nav} setNav = {setNav}/>
+            <div className="w-full h-[1000px]">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/d/d2/Robert_Young_Vineyards.jpg" className="flex mx-auto max-w-[700px]"/>
+
+            </div>
+            <Footer />
         </div>
-      );
+    );
 }
  
 export default Ivents;
