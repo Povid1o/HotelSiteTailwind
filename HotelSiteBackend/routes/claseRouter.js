ac6d8de3552b88b61e18b0d8c00717eb5672663a
Kirei1 <mrnelsen1@gmail.com>
2024-07-15 18:56:44 +0300

    Фронт

--- HotelSiteBackend/routes/claseRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const claseContoller = require('../controllers/claseContoller')
-const checkRole = require('../middleware/checkRoleMiddleware')
-
-router.post('/', checkRole('ADMIN'), claseContoller.create)
-router.get('/', claseContoller.getAll)
-
-module.exports = router
