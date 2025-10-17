6dffa8748010fcede27a7443c4e5bc7f79754575
Pavel Ryzhkov <81442161+povilas1565@users.noreply.github.com>
2025-10-17 23:43:18 +0300

    Remove unused path import in index.js

--- HotelSiteBackend/index.js
+++ HotelSiteBackend/index.js
@@ -85,7 +85,6 @@ app.use('/api', router)
 
 // ---- Swagger ----
 const swaggerUi = require('swagger-ui-express');
-const path = require('path');
 const YAML = require('yamljs');
 
 const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
