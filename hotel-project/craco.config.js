e27865a341bbc3515fad85448759c322ac269079
Povid1o <nikitka.bashlykov@mail.ru>
2025-10-18 01:31:55 +0300

    test commit for ci/cd

--- hotel-project/craco.config.js
+++ hotel-project/craco.config.js
@@ -1,18 +1,8 @@
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      // Fix plyr module resolution
-      webpackConfig.resolve = {
-        ...webpackConfig.resolve,
-        alias: {
-          ...webpackConfig.resolve.alias,
-          // Force plyr to resolve to dist folder instead of src
-          'plyr': require.resolve('plyr/dist/plyr.js')
-        }
-      };
+      // Custom webpack configuration can be added here if needed
       return webpackConfig;
     }
   }
 };
-
-
