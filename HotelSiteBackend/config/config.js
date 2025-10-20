d5f40d37d96bb02cd240cd3052828ac687c5eee2
root <root@cv5276184.novalocal>
2025-10-20 15:21:28 +0300

    prod version

--- HotelSiteBackend/config/config.js
+++ HotelSiteBackend/config/config.js
@@ -8,5 +8,13 @@ module.exports = {
         host: process.env.DB_HOST || "localhost",
         port: process.env.DB_PORT || "5432",
         dialect: "postgres"
-    }
+    },
+    production: {
+    	username: process.env.DB_USER || "postgres",
+    	password: process.env.DB_PASSWORD || "root",
+    	database: process.env.DB_NAME || "hotel_site",
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || "5432",
+    	dialect: "postgres"
+  	}
 };
